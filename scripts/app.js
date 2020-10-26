2a1cd05a374ab2946dfd15fa659dd9daec6f1d47
Armando <llslopezarmando@csu.fullerton.edu>
2020-10-25 22:26:18 -0700

    push

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+// ONLINE: you MUST fill in the following values 
+//               from Firebase/Firestore for your project 
+// PROJECT_ID,
+// WEB_API_KEY,
+// PROJECT_NUMBER
+//
+// go to... 
+// console.Firebase.google.com, then 
+//    click on the Gear Icon -> Project Settings -> General
+(function (window) {
+    'use strict';
+    window.FireBaseConfig = {
+        apiKey: "WAK",
+        authDomain: "PID.firebaseapp.com",
+        databaseURL: "[PID].firebaseio.com",
+        projectId: "PID",
+        storageBucket: "PID.appspot.com",
+        messagingSenderId: "PNUM",
+    };
+})(window);
